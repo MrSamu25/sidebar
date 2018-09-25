a2154581ec3242a5139302fbd34562a51c6dd34e
Steven Muriel <31315089+MrSamu25@users.noreply.github.com>
2018-09-25 10:55:48 -0500

    Add files via upload
    Se expande parcial o totalmente cuando la ventana tiene == width 600px

--- sidebar/sidebarw3.js
+++ sidebar/sidebarw3.js
@@ -1,12 +1,42 @@
 function abrir_cerrar() {
-    if(document.getElementById("mySidebar").style.display == "none") {
+
+    //La ventana es de tamaño 600px o menos
+    var x = window.matchMedia("(max-width: 600px)")
+
+    /* Abrir side-bar toda la pantalla*/
+    if (document.getElementById("mySidebar").style.display == "none" && x.matches) {
+        document.getElementById("contenido").style.marginLeft = "22%";
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("mySidebar").style.display = "block";
+        console.log(" /* Abrir side-bar toda la pantalla*/ ");
+        console.log("tamaño ventana mayora 600px ");
+    }
+
+    /* Abrir side-bar parte de la pantalla */
+    else if (document.getElementById("mySidebar").style.display == "none") {
         document.getElementById("contenido").style.marginLeft = "22%";
         document.getElementById("mySidebar").style.width = "20%";
-        document.getElementById("mySidebar").style.display = "block";      
+        document.getElementById("mySidebar").style.display = "block";
+        console.log(" /* Abrir side-bar parte de la pantalla */ ");
     }
-    else{
+
+    /* Cerrar side-bar  */
+    else {
+        document.getElementById("mySidebar").style.display = "none"
         document.getElementById("contenido").style.marginLeft = "2%";
-        document.getElementById("mySidebar").style.display = "none";
+        console.log(" /* Cerrar side-bar  */ ");
     }
 
 }
+/* 
+    if(document.getElementById("contenido").style.fontSize == " 8px" && document.getElementById("mySidebar").style.display == "none"){
+        document.getElementById("mySidebar").style.width = "100%";
+        document.getElementById("mySidebar").style.display = "block"
+    }
+
+
+    bien abajo
+    document.getElementById("contenido").style.marginLeft = "2%";
+    document.getElementById("mySidebar").style.display = "none";
+
+*/
